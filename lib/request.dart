878bc889b06fa6679344dfe7461853deced1abba
Chiesa Afandy <chiesaafandy@gmail.com>
2019-11-16 22:07:41 +0700

    Second

--- lib/request.dart
+++ lib/request.dart
@@ -7,11 +7,54 @@ class Request extends StatefulWidget {
 
 class _RequestState extends State<Request> {
   @override
+  
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:Scaffold(
-        body: Container(
-         
+      home:Scaffold(resizeToAvoidBottomInset: false,
+        appBar: AppBar(title: Text("Request"),),
+        body: Container(margin: EdgeInsets.all(20),
+         child: Column(
+           children: <Widget>[
+             Text("Title :"),
+           Container(child: TextFormField(decoration: InputDecoration(
+             hintText: "Insert Request Title"
+           ),
+             
+           ),),
+           Text("Company : "),
+           Container(child: TextField(decoration: InputDecoration(
+             hintText: "Insert Company Name"
+           ),
+
+           ),),
+
+           Text("Order By : "),
+           Container(child: TextField(decoration: InputDecoration(
+             hintText: "Admin Name"
+           ),
+
+           ),),
+
+           Text("Order Date : "),
+           Container(child: TextField(decoration: InputDecoration(
+             hintText: "Insert Order Date"
+           )
+
+           ),),
+           
+           Text("Description : "),
+           Container(child: TextField(keyboardType: TextInputType.multiline,
+            maxLines: null,
+
+           ),
+           ),Text("Order List"),
+
+           Container(
+             
+           )
+
+           ],
+         ),
         ),
       ),
     );
--- lib/requestList.dart
+++ lib/requestList.dart
@@ -13,52 +13,12 @@ class _RequestListState extends State<RequestList> {
         body: Container(
            child: ListView(
                   children: <Widget>[
-                    Card(child: Isi(title:"Request #1" ,admin: "Lorem" ,perusahaan: "Lorem Ipsum" ,tanggal: "13 april 2020" ,),)
+                    
                   ],
           ),
         ),
       ),
       
-    );
-  }
-}
-
-
-class Isi extends StatelessWidget {
-
-  Isi ({this.title, this.admin, this.perusahaan, this.tanggal,});
-
-  final String title, admin, perusahaan, tanggal;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: new Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          new Container(margin: EdgeInsets.fromLTRB(40, 40, 40, 40),
-              child: new Column(
-            children: <Widget>[
-              new Text(
-                title,
-                style: new TextStyle(fontSize: 21,fontWeight: FontWeight.bold),
-              ),
-              new Text(
-                admin,
-                style: new TextStyle(fontSize: 18,),
-              ),
-              new Text(
-                perusahaan,
-                style: new TextStyle(fontSize: 18,),
-              ),
-              new Text(
-                tanggal,
-                style: new TextStyle(fontSize: 18,),
-              )
-            ],
-          ))
-        ],
-      ),
     );
   }
 }
